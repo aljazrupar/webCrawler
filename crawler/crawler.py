aca20b9f353320794634c9d53d5e899b41bb9e25
Gregor <gregor.azbe@gmail.com>
2020-03-21 20:59:55 +0100

    Dodana paralelizacija

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,9 +1,13 @@
 import argparse
 import concurrent.futures
 import datetime
+import socket
 import sys
 import threading
 import urllib
+from multiprocessing.context import Process
+from multiprocessing import Queue as MultiProcessingQueue
+from os import path
 from queue import Queue, Empty
 from urllib.request import urlparse, urljoin
 import urllib.robotparser
@@ -37,6 +41,27 @@ executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
 schemes = ["", "www", "http", "https"]
 lock = threading.Lock()
 visitedPages = set()
+workers = []
+
+class Worker:
+    def __init__(self, index):
+        self.index = index
+        self.currentIp = ""
+        self.queue = MultiProcessingQueue()
+
+        self.p = Process(target=crawlNext, args=(self,))
+        self.p.start()
+
+    def join(self):
+        self.p.join()
+
+    def set_current_ip(self, ip):
+        with(worker_ip_lock):
+            self.currentIp = ip
+
+    def get_current_ip(self):
+        with(worker_ip_lock):
+            return self.currentIp
 
 #TA ZADEVA ŠE NE DELA ČISTO, KER VRNE NEK ID NAMEST URL-JA.. MORDA JE TREBA NAREDIT DRUGAČE
 #--------------------------------------------
@@ -101,24 +126,25 @@ def get_all_website_links(url,id_of_new_page, crawlDelay): # najde vse linke na 
     img_tags = soup.find_all('img')
 
     ImgUrls = [img['src'] for img in img_tags]
-    cur = conn.cursor()
-    for imgUrl in ImgUrls:
+    with lock:
+        cur = conn.cursor()
+        for imgUrl in ImgUrls:
 
-        # zdruzi url z domeno ce ni ze cel link.
-        imgCleanUrl = urljoin(url, imgUrl)
-        parsed_img = urlparse(imgCleanUrl)
+            # zdruzi url z domeno ce ni ze cel link.
+            imgCleanUrl = urljoin(url, imgUrl)
+            parsed_img = urlparse(imgCleanUrl)
 
-        # sparsa link
-        imgCleanUrl = parsed_img.scheme + "://" + parsed_img.netloc + parsed_img.path
-        filename = imgCleanUrl.split("/")[-1]
-        content_type = filename.split(".")[-1]
+            # sparsa link
+            imgCleanUrl = parsed_img.scheme + "://" + parsed_img.netloc + parsed_img.path
+            filename = imgCleanUrl.split("/")[-1]
+            content_type = filename.split(".")[-1]
 
-        if parsed_img.scheme in schemes:
-            response = requests.get(imgCleanUrl)
-            time.sleep(crawlDelay)
-            cur.execute("INSERT INTO crawldb.image VALUES(DEFAULT,%s, %s, %s,%s ,CURRENT_TIMESTAMP)",
-                (id_of_new_page, filename, content_type, "BINARY"))
-    cur.close()
+            if parsed_img.scheme in schemes:
+                response = requests.get(imgCleanUrl)
+                time.sleep(crawlDelay)
+                cur.execute("INSERT INTO crawldb.image VALUES(DEFAULT,%s, %s, %s,%s ,CURRENT_TIMESTAMP)",
+                    (id_of_new_page, filename, content_type, "BINARY"))
+        cur.close()
 
     return urls
 
@@ -136,65 +162,91 @@ def scraper(url, id_of_new_site, crawlDelay):  # Funkcija za obdelovanje ene str
 
     #Preverjamo ali je HTML, Binary ali duplikat
     if 'html' in response.headers['content-type']:
-        #with lock:
+        with lock:
+            cur.execute("SELECT id FROM crawldb.page WHERE url=" + "'" + url + "'")
+            data = cur.fetchone()
+            if not data:
 
-        cur.execute("SELECT id FROM crawldb.page WHERE url=" + "'" + url + "'")
-        data = cur.fetchone()
-        if not data:
+                cur.execute("SELECT hash, id FROM crawldb.page")
+                hashesUrls = cur.fetchall()
 
-            cur.execute("SELECT hash, id FROM crawldb.page")
-            hashesUrls = cur.fetchall()
+                if not hashesUrls:
+                    hashes = []
+                else:
+                    hashes = [x[0] for x in hashesUrls]
+                    ids = [y[1] for y in hashesUrls]
 
-            if not hashesUrls:
-                hashes = []
+                hash = hashlib.md5(html_content.encode()).hexdigest()
+
+                if hash in hashes:
+                    #Če je duplikat:
+                    cur.execute("INSERT INTO crawldb.page VALUES(DEFAULT,%s, %s, %s,%s ,%s ,%s ,CURRENT_TIMESTAMP ) RETURNING id",
+                            (id_of_new_site, "DUPLICATE", url, "NULL",  status_code, hash))
+                    id_of_new_page = cur.fetchone()[0]
+
+                    cur.execute("INSERT INTO crawldb.link VALUES(%s, %s)",
+                        (id_of_new_site, ids[hashes.index(hash)]))
+
+                else:
+                    #Če je originalna stran
+                    cur.execute("INSERT INTO crawldb.page VALUES(DEFAULT,%s, %s, %s,%s ,%s ,%s ,CURRENT_TIMESTAMP ) RETURNING id",
+                        (id_of_new_site, "HTML", url, html_content, status_code, hash))
+                    id_of_new_page = cur.fetchone()[0]
             else:
-                hashes = [x[0] for x in hashesUrls]
-                ids = [y[1] for y in hashesUrls]
-
-            hash = hashlib.md5(html_content.encode()).hexdigest()
-
-            if hash in hashes:
-                #Če je duplikat:
-                cur.execute("INSERT INTO crawldb.page VALUES(DEFAULT,%s, %s, %s,%s ,%s ,%s ,CURRENT_TIMESTAMP ) RETURNING id",
-                        (id_of_new_site, "DUPLICATE", url, "NULL",  status_code, hash))
-                id_of_new_page = cur.fetchone()[0]
-
-                cur.execute("INSERT INTO crawldb.link VALUES(%s, %s)",
-                    (id_of_new_site, ids[hashes.index(hash)]))
-
-            else:
-                #Če je originalna stran
-                cur.execute("INSERT INTO crawldb.page VALUES(DEFAULT,%s, %s, %s,%s ,%s ,%s ,CURRENT_TIMESTAMP ) RETURNING id",
-                    (id_of_new_site, "HTML", url, html_content, status_code, hash))
-                id_of_new_page = cur.fetchone()[0]
-        else:
-            id_of_new_page = data[0]
+                id_of_new_page = data[0]
 
     else:
-        #with lock:
-        #Če je Binary naprimer slika, pdf,...
-        cur.execute("INSERT INTO crawldb.page VALUES(DEFAULT, %s, %s, %s, %s, %s, %s, ,CURRENT_TIMESTAMP ) RETURNING id",
-                        (id_of_new_site, 'BINARY', url, "NULL",  status_code, "NULL" ))
-        id_of_new_page = cur.fetchone()[0]
-        response = requests.get(url)
-        time.sleep(crawlDelay)
-        cur.execute("INSERT INTO crawldb.page_data VALUES(DEFAULT,%s, %s,%s ,%s )",
-                    (id_of_new_page,  response.headers['content-type'], "BINARY"))
+        with lock:
+            #Če je Binary naprimer slika, pdf,...
+            cur.execute("INSERT INTO crawldb.page VALUES(DEFAULT, %s, %s, %s, %s, %s, %s, ,CURRENT_TIMESTAMP ) RETURNING id",
+                            (id_of_new_site, 'BINARY', url, "NULL",  status_code, "NULL" ))
+            id_of_new_page = cur.fetchone()[0]
+            # response = requests.get(url)
+            # time.sleep(crawlDelay)
+            cur.execute("INSERT INTO crawldb.page_data VALUES(DEFAULT,%s, %s,%s ,%s )",
+                        (id_of_new_page,  response.headers['content-type'], "BINARY"))
 
-        cur.close()
+            cur.close()
     #Dodamo linke
     urls = get_all_website_links(url, id_of_new_page, crawlDelay)
     for curr_url in urls:
         queue.put(curr_url)
-
+    '''
     if(not queue.empty()):
         crawlNext()
     else:
         return
+    '''
 
-def crawlNext():
+def crawlNext(my_worker):
     #Funkcija gre po vrsti in obdeluje urlje enega po enega
-    target_url = queue.get(block=True)
+    target_url = ""
+    while target_url == "":
+        with(lock):
+            # for worker in workers
+            if not my_worker.queue.empty():
+                target_url = my_worker.queue.get()
+            else:
+                my_worker.currentIp = ""
+                while not queue.empty():
+                    target_url = queue.get(block=True)
+
+                    ip = socket.gethostbyname(target_url)
+                    for worker in workers:
+                        if my_worker.currentIp == ip:
+                            worker.queue.put(target_url)
+                            break
+                    else:
+                        break
+                else: # v vrsti ni še nobenega url-ja, ki bi ga worker lahko obdelal
+                    target_url = ""
+        time.sleep(5)
+        for worker in workers:
+            if not worker.currentIp == "":
+                break
+        else:
+            return
+
     print(target_url)
     if target_url not in visited:
 
@@ -203,38 +255,38 @@ def crawlNext():
         #executor.submit(checkPermissions, target_url)
 
         checkPermissions(target_url)
-    crawlNext()
+    crawlNext(my_worker)
     if (queue.empty()):
         return
 
 
 def addSiteToDB(base_url, rp, robotsURL):
     #Funkcija doda novo domeno v bazo
-
-    cur = conn.cursor()
-    cur.execute("SELECT 1 FROM crawldb.site WHERE domain=" + "'" + base_url + "'")
-    data = cur.fetchall()
-    if not data:
-
-        if rp != "":
-            listSite_Maps = rp.site_maps()
-            textRobots = urllib.request.urlopen(robotsURL).read().decode("utf-8")
-        else:
-            listSite_Maps = ""
-            textRobots = ""
-        cur.execute("INSERT INTO crawldb.site VALUES(DEFAULT,%s, %s, %s) RETURNING id",
-            (base_url, textRobots, listSite_Maps))
-
-        id_of_new_site = cur.fetchone()[0]
-
-        cur.close()
-
-    else:
-        #Ce je domenska stran ze notri jo najdemo in vrnemo index
+    with lock:
         cur = conn.cursor()
-        cur.execute("SELECT id FROM crawldb.site WHERE domain=" + "'" + base_url + "'")
+        cur.execute("SELECT 1 FROM crawldb.site WHERE domain=" + "'" + base_url + "'")
+        data = cur.fetchall()
+        if not data:
 
-        id_of_new_site = cur.fetchone()[0]
+            if rp != "":
+                listSite_Maps = rp.site_maps()
+                textRobots = urllib.request.urlopen(robotsURL).read().decode("utf-8")
+            else:
+                listSite_Maps = ""
+                textRobots = ""
+            cur.execute("INSERT INTO crawldb.site VALUES(DEFAULT,%s, %s, %s) RETURNING id",
+                (base_url, textRobots, listSite_Maps))
+
+            id_of_new_site = cur.fetchone()[0]
+
+            cur.close()
+
+        else:
+            #Ce je domenska stran ze notri jo najdemo in vrnemo index
+            cur = conn.cursor()
+            cur.execute("SELECT id FROM crawldb.site WHERE domain=" + "'" + base_url + "'")
+
+            id_of_new_site = cur.fetchone()[0]
 
     return id_of_new_site
 
@@ -290,6 +342,12 @@ if __name__ == '__main__':
 
     for seed in seed_urls:
         queue.put(seed)
-    crawlNext()
+    # crawlNext()
+    n_workers = 1
+    for i in range(n_workers):
+        workers += [Worker(i)]
+
+    for worker in workers:
+        worker.join()
 
     conn.close()
